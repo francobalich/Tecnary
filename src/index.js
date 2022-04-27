752c55f1ffbe9db91ce88006e8a75c746f0ff696
franco.balich@gmail.com <franco.balich@gmail.com>
2022-04-26 22:07:59 -0300

    Add json db

--- src/index.js
+++ src/index.js
@@ -5,22 +5,13 @@ const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const fs = require("fs");
+const JSONdb = require("simple-json-db");
+const dbPuntajes = new JSONdb("./src/json/lista.json");
+const dbPreguntas = new JSONdb("./src/json/preguntas.json");
 
-const preguntas = JSON.parse(
-  fs.readFileSync(path.join(__dirname, "..", "src", "json", "preguntas.json"))
-);
-const puntajes = JSON.parse(
-  fs.readFileSync(path.join(__dirname, "..", "src", "json", "lista.json"))
-);
 const iniciarServer = () => {
   server.listen(port, () => {
-    console.log(
-      "\u001b[" +
-        33 +
-        "m" +
-        `El server esta corriendo el puerto:${port}` +
-        "\u001b[0m"
-    );
+    console.log(`El server esta corriendo el puerto:${port}`);
   });
   var publicPath = path.resolve(__dirname, "../public");
   app.use(express.static(publicPath));
@@ -29,42 +20,36 @@ const iniciarServer = () => {
   });
 };
 
- const genRandomNumer= ()=>{
-     return Math.floor(Math.random() * (8 - 0));
- }
-
+const genRandomNumer = () => {
+  return Math.floor(Math.random() * (8 - 0));
+};
 io.on("connection", (socket) => {
   socket.on("pregunta", (data) => {
     if (data == "consulta") {
-      let numA = genRandomNumer()
-      let numB = genRandomNumer()
-      let numC = genRandomNumer()
-      let numD = genRandomNumer()
+      let numA = genRandomNumer();
+      let numB = genRandomNumer();
+      let numC = genRandomNumer();
+      let numD = genRandomNumer();
       //console.log("Se hizo la pregunta");
       let jsonDemo = {
-        
-          respuestaFalsas:
-            [`${preguntas[numB].respuesta}`,`${preguntas[numC].respuesta}`,`${preguntas[numD].respuesta}`]
-        
+        respuestaFalsas: [
+          `${dbPreguntas.get(numB).respuesta}`,
+          `${dbPreguntas.get(numC).respuesta}`,
+          `${dbPreguntas.get(numD).respuesta}`,
+        ],
       };
-      let jsonToSend=Object.assign(preguntas[numA], jsonDemo)
+      let jsonToSend = Object.assign(dbPreguntas.get(numA), jsonDemo);
       io.emit("respuesta", jsonToSend);
     }
   });
-  socket.on("user",(data)=>{
-    if(puntajes[data]!=undefined){
-      socket.emit("puntaje",puntajes[data])
+  socket.on("user", (data) => {
+    if (dbPuntajes.has(data)) {
+      socket.emit("puntaje", dbPuntajes.get(data));
     }
-  })
-  socket.on("refreshPoints",(data)=>{
-    console.log(data)
-    puntajes[String(data[0])]=String(data[1])
-    fs.writeFile(path.join(__dirname, "..", "src", "json", "lista.json"), JSON.stringify(puntajes), err => {
-      if (err) {
-        console.error(err)
-        return
-      }
-    })
-  })
+  });
+  socket.on("refreshPoints", (data) => {
+    console.log(data);
+    dbPuntajes.set(data[0], data[1]);
+  });
 });
 iniciarServer();
